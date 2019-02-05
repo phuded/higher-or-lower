b3dd9c0368473c5613b3925b4c36cc2a28fd2ed5
Matt Carter <mcarter@ooyala.com>
2019-02-05 19:48:24 +0000

    Try again

--- public/js/scores.js
+++ public/js/scores.js
@@ -8,51 +8,30 @@ $.getCopyUrl = function(){
     url = url.join("/");
 
     copyTextToClipboard(url);
-}
+};
 
 //TODO: make util class
 function copyTextToClipboard(text) {
 
-    var textArea = document.createElement("textarea");
+    var textArea, range, selection;
 
-    // Place in top-left corner of screen regardless of scroll position.
-    textArea.style.position = 'fixed';
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-
-    // Ensure it has a small width and height. Setting to 1px / 1em
-    // doesn't work as this gives a negative w/h on some browsers.
-    textArea.style.width = '2em';
-    textArea.style.height = '2em';
-
-    // We don't need padding, reducing the size if it does flash render.
-    textArea.style.padding = 0;
-
-    // Clean up any borders.
-    textArea.style.border = 'none';
-    textArea.style.outline = 'none';
-    textArea.style.boxShadow = 'none';
-
-    // Avoid flash of white box if rendered for any reason.
-    textArea.style.background = 'transparent';
-
-
+    textArea = document.createElement('textArea');
     textArea.value = text;
-
     document.body.appendChild(textArea);
-    textArea.focus();
+
+    range = document.createRange();
+    range.selectNodeContents(textArea);
+    selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    textArea.setSelectionRange(0, 999999);
+
     textArea.select();
 
-    try {
-
-        document.execCommand('copy');
-
-    } catch (err) {
-
-    }
-
+    document.execCommand('copy');
     document.body.removeChild(textArea);
-}
+
+};
 
 //Update the score for a player
 $.updateScore = function(_players, fingersToDrink, skipHighScores){
